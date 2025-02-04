d6188f6ddcb1c8b86bbbbf1bf5c1e5503fb93c2c
Dylan Van Assche <me@dylanvanassche.be>
2025-02-04 11:42:52 +0000

    Revert "test-cases: add HTML view (#58)"
    This reverts commit a20a2b16cfb70859c7637b9834f5a93140cd83db.

--- test-cases/list.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-for i in RML*; do
-	echo "<section id=\"$i\" data-include=\"$i/README.md\" data-include-format="markdown"></section>"
-done
--- test-cases/section/abstract.md
+++
@@ -1,1 +0,0 @@
-This document defines the RML-LV test cases to the determine the RML-LV specification conformance of tools.
